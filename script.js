f7287fdfa0015b23c2e0edc8e5e93a37bc5a5b4f
Quevon Montojo Galang <91044904+quevon@users.noreply.github.com>
2023-07-04 20:22:22 -0700

    added modal message

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const input = document.getElementById("textInput");
 const result = document.getElementById("result");
 const modal = document.getElementById("myModal");
+const modalMessage = document.getElementById("message");
+const modalContent = document.querySelector(".modal-content");
+
 const closeModal = document.getElementsByClassName("close")[0];
 
 input.addEventListener("input", function () {
@@ -8,9 +11,22 @@ input.addEventListener("input", function () {
 });
 
 function copyText() {
+  const value = input.value;
   const textToCopy = result.innerText;
-  navigator.clipboard.writeText(textToCopy);
-  modal.style.display = "block";
+  if (value === null || value.trim() === "") {
+    modal.style.display = "block";
+    modalContent.style.backgroundColor = "#ee6c6c";
+    modalMessage.innerText = "Input Text First!";
+    result.innerText = "";
+    input.value = "";
+  } else {
+    navigator.clipboard.writeText(textToCopy);
+    modal.style.display = "block";
+    modalContent.style.backgroundColor = "#9ae6b5";
+    modalMessage.innerText = "Text Copied Succesfully!";
+    result.innerText = "";
+    input.value = "";
+  }
 }
 
 function clearText() {
