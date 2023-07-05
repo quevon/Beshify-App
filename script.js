dbf3f6685d15efa91272cb34c3be61ef282ca5e1
Quevon Montojo Galang <91044904+quevon@users.noreply.github.com>
2023-07-04 20:02:27 -0700

    add modal

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const input = document.getElementById("textInput");
 const result = document.getElementById("result");
+const modal = document.getElementById("myModal");
+const closeModal = document.getElementsByClassName("close")[0];
 
 input.addEventListener("input", function () {
   result.innerText = input.value.replace(/ /g, "🤸");
@@ -8,8 +10,20 @@ input.addEventListener("input", function () {
 function copyText() {
   const textToCopy = result.innerText;
   navigator.clipboard.writeText(textToCopy);
+  modal.style.display = "block";
 }
+
 function clearText() {
   result.innerText = "";
   input.value = "";
 }
+
+closeModal.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
